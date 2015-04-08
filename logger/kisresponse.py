e9f3ef0beeb334591c254db21a767c72f7a2223c
Gabe Torres <gtorres@bulogics.com>
2015-04-08 16:16:11 -0400

    refactored responses slightly

--- logger/kisresponse.py
+++ logger/kisresponse.py
@@ -5,61 +5,82 @@ logger = logging.getLogger()
 _response_types = []
 
 
-class KismetResponse(object):
-	'''Kismet response class'''
-
-	def __init__(self, string):
-		pass
-
-
 
 def response(cls):
 	_response_types.append(cls)
 	logger.debug("Adding response type %s" % cls.__name__)
 
 def from_raw(raw):
-	pass
-
+    ret = None
+    for restype in _response_types:
+        ret = restype.from_raw(raw)
+        if (ret is not None):
+            return ret
 
 @response
-class AckResponse(KismetResponse):
+class AckResponse(object):
 	'''Ack Response Type: *ACK ID'''
+
+    key = '*ACK'
+
+	def __init__(self, text):
+        self._text = text
+
 
 	@classmethod
 	def from_raw(cls, string):
-		#check if ACK
+        if (string.startswith(cls.key)):
+            return cls.__init__(string)
 
 @response
-class IntroResponse(KismetResponse):
+class IntroResponse(object):
 	'''Kismet Intro Response: *KISMET ETC'''
 
-	def __init__(self):
-		pass
+    key = '*KISMET'
+
+	def __init__(self, text):
+        self._text = text
+
+	@classmethod
+	def from_raw(cls, string):
+        return None
 
 @response
-class ErrorResponse(KismetResponse):
+class ErrorResponse(object):
+
+    key = '*ERROR'
+
 	'''Kismet Error Response: *ERROR'''
 	def __init__(self):
 		pass
 
 @response
-class TimeResponse(KismetResponse):
+class TimeResponse(object):
 	'''Time Response: *TIME'''
 
+    key = '*TIME'
+
 	def __init__(self):
 		pass
 
 
 @response
-class ProtocolsResponse(KismetResponse):
+class ProtocolsResponse(object):
 	'''Time Response: *PROTOCOLS ETC'''
 
-	def __init__(self):
-		pass
-@response
-class UnknownResponse(KismetResponse):
-	'''Unkown Response, not implemented'''
+    key = '*PROTOCOLS'
 
 	def __init__(self):
 		pass
+@response
+class UnknownResponse(object):
+
+	'''Unkown Response, not implemented, should always be last'''
+
+    key = '*'
+
+	def __init__(self):
+		pass
+
+
 
